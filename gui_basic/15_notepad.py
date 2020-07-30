f0a56cd02259f5d1d61eba3f53fcbcb1e9b5a9c2
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-07-30 23:05:39 +0900

    gui_quiz: notepad

---
+++ gui_basic/15_notepad.py
@@ -0,0 +1,53 @@
+import os
+from tkinter import *
+
+root = Tk()
+root.title("Windows Notepad")
+root.geometry("640x480+500+100")
+
+# 파일명
+filename="mynote.txt"
+
+
+def openFile():
+    if os.path.isfile(filename):
+        with open(filename, "r", encoding="UTF-8") as file:
+            text.delete("1.0", END)
+            text.insert(END, file.read())
+
+
+def saveFile():
+    with open(filename, "w", encoding="UTF-8") as file:
+        file.write(text.get("1.0", END))
+
+
+#메뉴 영역
+menu = Menu(root)
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="Open", command=openFile)
+menu_file.add_command(label="Save", command=saveFile)
+menu_file.add_separator()
+menu_file.add_command(label="Exit", command=root.quit)
+menu.add_cascade(label="File", menu=menu_file)
+
+menu_edit = Menu(menu, tearoff=0)
+menu_template= Menu(menu, tearoff=0)
+menu_view = Menu(menu, tearoff=0)
+menu_help = Menu(menu, tearoff=0)
+
+menu.add_cascade(label="Edit")
+menu.add_cascade(label="Template")
+menu.add_cascade(label="View", menu=menu_view)
+menu.add_cascade(label="Help", menu=menu_help)
+
+# 본문 영역
+scroll = Scrollbar(root)
+text = Text(root, yscrollcommand=scroll.set)
+text.pack(side="left", fill="both", expand=True)
+
+# 스크롤바
+scroll.pack(side="right", fill="y")
+scroll.config(command=text.yview)
+
+root.config(menu=menu)
+root.mainloop()
